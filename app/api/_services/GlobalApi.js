70c5fcf6035e258e6ad7a6044a323cd355c4f2e6
shabeer_sf <shabeermen@gmail.com>
2024-10-29 21:58:18 +0530

    ui-text

--- app/api/_services/GlobalApi.js
+++ app/api/_services/GlobalApi.js
@@ -1,11 +1,7 @@
 const { default: axios } = require("axios");
-
 const SearchUser = (data) => axios.post("/api/search", data);
 const SignUpUser = (data) => axios.post("/api/signup", data);
-const FetchSubtopics = (data) => axios.post("/api/subtopics", data);
-
 export default {
   SearchUser,
   SignUpUser,
-  FetchSubtopics,
 };
