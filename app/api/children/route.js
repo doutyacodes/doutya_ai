b7ea5d543c387d6b54143508725edd14146a66bd
shabeer_sf <shabeermen@gmail.com>
2024-10-30 07:42:23 +0530

    ui-text

--- app/api/children/route.js
+++ app/api/children/route.js
@@ -7,20 +7,21 @@ import { authenticate } from "@/lib/jwtMiddleware";
 export async function GET(req) {
     const authResult = await authenticate(req);
     if (!authResult.authenticated) {
-      return authResult.response; // Return the response if authentication fails
+        return authResult.response; // Return the response if authentication fails
     }
     
     const userId = authResult.decoded_Data.id;
-  try {
-    const children = await db
-      .select()
-      .from(CHILDREN)
-      .where(eq(CHILDREN.user_id, userId))
-      .execute();
-// console.log(children)
-    return NextResponse.json({ data: children });
-  } catch (error) {
-    console.error("Fetch Children Error:", error);
-    return NextResponse.json({ message: "Server error" }, { status: 500 });
-  }
+
+    try {
+        const children = await db
+            .select()
+            .from(CHILDREN)
+            .where(eq(CHILDREN.user_id, userId))
+            .execute();
+
+        return NextResponse.json({ data: children });
+    } catch (error) {
+        console.error("Fetch Children Error:", error);
+        return NextResponse.json({ message: "Server error" }, { status: 500 });
+    }
 }
