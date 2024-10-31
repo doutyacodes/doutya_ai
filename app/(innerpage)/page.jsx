70745e335a65d64e966b27fb839c8f5b52d560c0
shabeer_sf <shabeermen@gmail.com>
2024-10-31 12:42:41 +0530

    ui-text

--- app/(innerpage)/page.jsx
+++ app/(innerpage)/page.jsx
@@ -162,7 +162,7 @@ const Home = () => {
                     value={type}
                     className="bg-transparent ring-transparent border focus:ring-0 focus-visible:ring-0 border-transparent underline decoration-2 underline-offset-4" // Thicker underline with offset
                   >
-                    <SelectTrigger className="w-fit ring-transparent border border-transparent focus-visible:ring-transparent bg-transparent md:text-4xl text-[35px] uppercase rounded-full p-2 focus:ring-0 focus-visible:ring-0 text-white underline decoration-2 underline-offset-4 [&>svg]:w-6 [&>svg]:h-24 [&>svg]:opacity-100">
+                    <SelectTrigger className="w-fit ring-transparent border border-transparent focus-visible:ring-transparent bg-transparent md:text-4xl text-[35px] uppercase rounded-full p-2 focus:ring-0 focus-visible:ring-0 text-white underline decoration-2 underline-offset-4 [&>svg]:w-8 [&>svg]:h-24 [&>svg]:opacity-100">
                       <SelectValue placeholder="Story" className="text-black" />
                     </SelectTrigger>
                     <SelectContent>
