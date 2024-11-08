8db3220b1f10678c8a258fec1ee66971033bfc2c
shabeer_sf <shabeermen@gmail.com>
2024-11-09 03:21:15 +0530

    ui-text

--- app/(innerpage)/tests/(pages)/myResults/page.js
+++ app/(innerpage)/tests/(pages)/myResults/page.js
@@ -1,12 +1,8 @@
 import React from 'react'
 import Results from '../../_components/Results/page'
-import dynamic from 'next/dynamic';
 
 export default function page() {
-  const MobileNavigation = dynamic(
-    () => import("../../_components/Navbar/button.jsx"),
-    { ssr: false }
-  );
+ 
   return (
     <div>
         <Results/>
