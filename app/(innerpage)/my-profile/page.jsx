42c38ce08f1a214277e0cfe8dcf1130334906222
shabeer_sf <shabeermen@gmail.com>
2024-11-14 09:44:18 +0530

    ui-text

--- app/(innerpage)/my-profile/page.jsx
+++ app/(innerpage)/my-profile/page.jsx
@@ -260,7 +260,7 @@ const Profile = () => {
           >
             <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
               <h3 className="text-2xl font-semibold mb-4">
-                Edit {editingChild.name}'s Info
+                Edit {editingChild.name} Info
               </h3>
               <div className="space-y-4">
                 <div>
