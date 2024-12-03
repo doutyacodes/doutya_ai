41099403612fd8fd3ad85cfa188f1b25e5612c4a
shabeer_sf <shabeermen@gmail.com>
2024-12-03 16:03:54 +0530

    first commit

--- app/_components/AgeSelectionPopup.jsx
+++ app/_components/AgeSelectionPopup.jsx
@@ -1,4 +1,5 @@
 "use client";
+
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
@@ -18,19 +19,11 @@ const AgeSelectionPopup = ({ onSubmit, onClose }) => {
   return (
     <div className="fixed inset-0 h-screen z-[999999999999] bg-gradient-to-br from-orange-200 via-white to-orange-100 bg-opacity-40">
       <div className="flex justify-center items-center">
-        <Link href={"/"} className=" flex justify-center items-center">
+        <Link href={"/"} className="flex justify-center items-center">
           <Image src={"/images/logo2.png"} width={90} height={120} alt="logo" />
         </Link>
       </div>
-      <div className=" flex items-center justify-center h-full ">
-        {/* Background overlay */}
-        {/* <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          className="fixed inset-0 bg-gradient-to-br from-orange-200 via-white to-orange-100"
-        /> */}
-
+      <div className="flex items-center justify-center h-full">
         {/* Popup container */}
         <motion.div
           initial={{ scale: 0.8, opacity: 0 }}
@@ -40,15 +33,28 @@ const AgeSelectionPopup = ({ onSubmit, onClose }) => {
           className="relative z-10 bg-white rounded-xl shadow-xl p-6 w-[90%] max-w-sm"
         >
           <h2 className="text-2xl font-bold text-gray-800 mb-4 text-center">
-            Enter your child&apos;s age
+            Select your child&apos;s age
           </h2>
-          <input
-            type="number"
-            value={age}
-            onChange={(e) => setAge(e.target.value)}
-            className="border-2 border-orange-300 focus:border-orange-500 rounded-lg px-4 py-2 w-full placeholder:text-center text-gray-700 focus:outline-none transition duration-200"
-            placeholder="Enter age (3-12) "
-          />
+          
+          {/* Standard select input with Tailwind styling */}
+          <div className="mb-6">
+            
+            <select
+              id="age"
+              name="age"
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+              className="w-full border-2 border-orange-300 rounded-lg px-4 py-2 text-gray-700 focus:outline-none focus:ring-2 focus:ring-orange-400"
+            >
+              <option value="" disabled>Age (3-12)</option>
+              {Array.from({ length: 10 }, (_, i) => (
+                <option key={i} value={i + 3}>
+                  {i + 3}
+                </option>
+              ))}
+            </select>
+          </div>
+
           <div className="flex justify-between mt-6 space-x-4">
             <motion.button
               whileHover={{ scale: 1.1 }}
