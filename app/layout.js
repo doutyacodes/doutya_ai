a7e28622f88f4a54f6c0bac083f31f2a115333ff
shabeer_sf <shabeermen@gmail.com>
2024-12-20 17:20:02 +0530

    first commit

--- app/layout.js
+++ app/layout.js
@@ -3,15 +3,15 @@ import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import { NextIntlClientProvider } from "next-intl";
 import { getLocale, getMessages } from "next-intl/server";
+import Head from "next/head"; // Import Head to manage HTML head metadata
 
 const poppins = Poppins({
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700","800"], // Choose the weights you want to include
+  weight: ["400", "500", "600", "700", "800"], // Choose the weights you want to include
 });
 
 export const metadata = {
-  title:
-    "Axara | News for kids.",
+  title: "Axara | News for kids.",
   description:
     "Axara is an AI-powered educational platform for children aged 2-12, offering personalized stories, interactive learning, and fun explanations on any topic. Designed to fuel curiosity and learning through play, Axara adapts to each child's unique needs, helping them explore the world in an engaging, age-appropriate way.",
 };
@@ -19,10 +19,23 @@ export const metadata = {
 export default async function RootLayout({ children }) {
   const locale = await getLocale();
   const messages = await getMessages();
+
   return (
     <html lang={locale}>
+      <Head>
+        {/* Favicon and icons */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+
+        {/* Meta tags for social media and SEO */}
+        <meta name="theme-color" content="#ffffff" />
+        <meta property="og:image" content="https://axaranews.com/favicon.ico" />
+        <meta name="description" content={metadata.description} />
+      </Head>
       <body className={`${poppins.className} min-h-screen`}>
-        
         <NextIntlClientProvider messages={messages}>
           <Toaster />
           {children}
