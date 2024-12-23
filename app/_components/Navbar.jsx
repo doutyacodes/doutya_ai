d885b48c97fca96e54992b8283576b7bf7d5756a
shabeer_sf <shabeermen@gmail.com>
2024-12-23 13:29:53 +0530

    first commit

--- app/_components/Navbar.jsx
+++ app/_components/Navbar.jsx
@@ -72,7 +72,7 @@ const Navbar = () => {
               className="mx-auto flex justify-center items-center relative h-[7.6vh] w-[40vw]"
             >
               <Image
-                src={pathname.includes("news") ? "/images/logo5.png" : "/images/logo4.png"}
+                src={pathname.includes("news") ? "/images/logo5.png" :pathname.includes("search") ? "/images/logo6.png" : "/images/logo4.png"}
                 fill
                 objectFit="contain"
                 alt="logo"
